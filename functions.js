f6c131afc1f0ad2339582a60ebf60b2a33773cc8
studtler <93228874+studtler@users.noreply.github.com>
2023-11-13 11:50:17 -0500

    added information for testing purposes

---
+++ functions.js
@@ -0,0 +1,11 @@
+let rps = ["rocks", "paper", "scissor"];
+
+function myFunction() { 
+    document.getElementById("demo").innerHTML = "mama dukes";
+}
+
+console.log(Math.floor(3.95));
+
+function computerChoices(rock, paper, scissor) { 
+
+}
